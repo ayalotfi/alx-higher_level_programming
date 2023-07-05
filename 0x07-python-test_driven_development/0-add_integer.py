c653b96cb8bd0423d4f9620853b207bd2d6a72fa
aya lotfi <ayalotfi59@gmail.com>
2023-07-05 14:05:46 -0700

    Integers addition

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-"""Module for add_integer method"""
-
+"""Module for add int method """
 
 def add_integer(a, b=98):
-    """Function to add two integers"""
+    """ Function to add two int"""
 
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
@@ -12,4 +11,5 @@ def add_integer(a, b=98):
 
     a = int(a)
     b = int(b)
-    return a + b
+
+    return (a + b)
--- 0x07-python-test_driven_development/README.md
+++ 0x07-python-test_driven_development/README.md
@@ -1,1 +1,1 @@
-Python - Test-driven development
+python test driven development
