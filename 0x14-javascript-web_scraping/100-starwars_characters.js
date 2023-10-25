f5eca0a0e8abaf428b45433b850284bdeeaf246a
aya lotfi <ayalotfi59@gmail.com>
2023-10-24 18:05:34 -0700

    ss

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,22 @@
+
+Executable File·17 lines (16 loc) · 474 Bytes
+Code 55% faster with GitHub Copilot
+Raw
+#!/usr/bin/node
+const request = require('request');
+const url = 'http://swapi.co/api/films/' + process.argv[2];
+
+request(url, function (err, response, body) {
+  if (err == null) {
+    const resp = JSON.parse(body);
+    const characters = resp.characters;
+    for (let i = 0; i < characters.length; i++) {
+      request(characters[i], function (err, response, body) {
+        if (err == null) {
+          console.log(JSON.parse(body).name);
+        }
+      });
+    }
+  }
+});
+
